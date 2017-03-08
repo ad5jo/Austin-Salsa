fb3efd8f67bc559cbf3e70fd90b0017fac255fca
Dave Durkee <ad5jo@yahoo.com>
2017-03-08 09:09:46 -0600

    changed up the look adding a dance background. added Raul's webpage link

---
+++ assets/javascript/myjavascript.js
@@ -0,0 +1,10 @@
+// http://freemusicarchive.org/genre/Salsa/
+
+var s_songs = {
+	"SONGO_21_-_02_-_La_Mujer_de_Antonio",
+	"Frank_Guerrero_-_04_-_Barranquilla_Tiene_Un__Swing",
+	"Frank_Guerrero_-_03_-_Cupido",
+	"Frank_Guerrero_-_02_-_Fui_Yo",
+	"arsonist_-_01_-_Hot_salsa_trip"
+};
+
